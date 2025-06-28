b37882fcb720fac19f88207e4ec37f360381377a
Nehemiah Medubi <ayodelemedubi01@gmail.com>
2025-06-28 05:54:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/contact', async (req, res) => {
